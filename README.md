b50695fefa6c3a68e1f5a8cd89b50d64726497e6
Behfar Khosravi <42641836+behfarkhosravi@users.noreply.github.com>
2024-05-23 13:43:35 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# A terminal style website
+
+Forked from [M4TT72 | Terminal](https://term.m4tt72.com)
+
+## Quick Start
+
+### Using npm/yarn
+
+1. Install dependencies:
+
+```bash
+yarn install
+```
+
+2. Run the server:
+
+```bash
+yarn dev
+```
+
+## Contributing
+
+Please feel free to pull requests or log issues.
+
+Thanks!
+
---
+++ src/utils/commands.ts
@@ -0,0 +1,116 @@
+import packageJson from '../../package.json';
+import themes from '../../themes.json';
+import { history } from '../stores/history';
+import { theme } from '../stores/theme';
+
+const hostname = window.location.hostname;
+
+export const commands: Record<string, (args: string[]) => Promise<string> | string> = {
+  help: () => 'Available commands: ' + Object.keys(commands).join(', '),
+  hostname: () => hostname,
+  date: () => new Date().toLocaleString(),
+  echo: (args: string[]) => args.join(' '),
+  whoami: () => {
+    window.open('behfarkhosravi.pdf');
+
+    return `Behfar Khosravi`;
+  },
+  theme: (args: string[]) => {
+    const usage = `Usage: theme [args].
+    [args]:
+      ls: list all available themes
+      set: set theme to [theme]
+
+    [Examples]:
+      theme ls
+      theme set gruvboxdark
+    `;
+    if (args.length === 0) {
+      return usage;
+    }
+
+    switch (args[0]) {
+      case 'ls': {
+        let result = themes.map((t) => t.name.toLowerCase()).join(', ');
+        result;
+
+        return result;
+      }
+
+      case 'set': {
+        if (args.length !== 2) {
+          return usage;
+        }
+
+        const selectedTheme = args[1];
+        const t = themes.find((t) => t.name.toLowerCase() === selectedTheme);
+
+        if (!t) {
+          return `Theme '${selectedTheme}' not found. Try 'theme ls' to see all available themes.`;
+        }
+
+        theme.set(t);
+
+        return `Theme set to ${selectedTheme}`;
+      }
+
+      default: {
+        return usage;
+      }
+    }
+  },
+  repo: () => {
+    window.open(packageJson.repository.url, '_blank');
+
+    return 'Opening repository...';
+  },
+  clear: () => {
+    history.set([]);
+
+    return '';
+  },
+  email: () => {
+    window.open(`mailto:${packageJson.author.email}`);
+
+    return `Opening mailto:${packageJson.author.email}...`;
+  },
+  weather: async (args: string[]) => {
+    const city = args.join('+');
+
+    if (!city) {
+      return 'Usage: weather [city]. Example: weather Brussels';
+    }
+
+    const weather = await fetch(`https://wttr.in/${city}?ATm`);
+
+    return weather.text();
+  },
+  exit: () => {
+    return 'Please close the tab to exit.';
+  },
+  curl: async (args: string[]) => {
+    if (args.length === 0) {
+      return 'curl: no URL provided';
+    }
+
+    const url = args[0];
+
+    try {
+      const response = await fetch(url);
+      const data = await response.text();
+
+      return data;
+    } catch (error) {
+      return `curl: could not fetch URL ${url}. Details: ${error}`;
+    }
+  },
+  banner: () => `
+  ██╗  ██╗██╗    ████████╗██╗  ██╗███████╗██████╗ ███████╗
+  ██║  ██║██║    ╚══██╔══╝██║  ██║██╔════╝██╔══██╗██╔════╝
+  ███████║██║       ██║   ███████║█████╗  ██████╔╝█████╗  
+  ██╔══██║██║       ██║   ██╔══██║██╔══╝  ██╔══██╗██╔══╝  
+  ██║  ██║██║       ██║   ██║  ██║███████╗██║  ██║███████╗ v${packageJson.version}
+
+Type 'help' to see list of available commands.
+`,
+};
